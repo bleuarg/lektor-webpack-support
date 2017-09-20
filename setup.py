3cd4b87ec8a3b533c109fc6aba01cbba853297c5
Patrick Davies <me@patrickdavies.ca>
2017-09-20 16:20:08 -0400

    Change plugin information.

--- setup.py
+++ setup.py
@@ -9,19 +9,19 @@ tests_require = [
 ]
 
 setup(
-    name='lektor-webpack-support',
-    author='Armin Ronacher',
-    author_email='armin.ronacher@active-4.com',
-    url='http://github.com/lektor/lektor-webpack-support',
-    version='0.3',
+    name='lektor-frontend-build',
+    author='Patrick Davies',
+    author_email='me@patrickdavies.ca',
+    url='https://github.com/bleuarg/lektor-webpack-support',
+    version='0.1.0',
     license='BSD',
     description='Adds support for webpack to Lektor',
-    py_modules=['lektor_webpack_support'],
+    py_modules=['lektor_frontend_build'],
     tests_require=tests_require,
     extras_require={'test': tests_require},
     entry_points={
         'lektor.plugins': [
-            'webpack-support = lektor_webpack_support:WebpackSupportPlugin',
+            'frontend-build-system = lektor_frontend_build:FrontendBuildPlugin',
         ]
     }
 )
